620f9391396b93ee96655c1b9657ea424bc153c2
liuc96 <wx_f01304e2f67947f095dc3ffcbc29c2c9@git.code.tencent.com>
2018-10-19 15:02:42 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: "./src/index.js",
+    another: "./src/another-module.js"
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Code Splitting"
+    })
+  ],
+  optimization: {
+    runtimeChunk: {
+      name: "common"
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "lodash",
+          chunks: "all"
+        }
+      }
+    }
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   }
 };
